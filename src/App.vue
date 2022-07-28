30e6a2663e138bf9d6d888a90f93afcae46853ac
wt <11220450+wt-wyj@user.noreply.gitee.com>
2022-07-28 10:45:54 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
