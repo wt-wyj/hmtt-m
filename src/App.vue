a4cbb45e4bb9422672c844f8e89f06c8051a420e
wt <11220450+wt-wyj@user.noreply.gitee.com>
2022-07-28 10:37:43 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    黑马头条
+    <i class="toutiao toutiao-wenda"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
